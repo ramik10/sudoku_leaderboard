5963d0540fe5dc5d3b62b03c2d8209e3354274d4
RAMIK MUKHERJEE <ramik.14062000@gmail.com>
2023-09-16 22:56:47 +0530

    init

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,22 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
 
+export function middleware() {
+    // retrieve the current response
+    const res = NextResponse.next()
 
-export const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    // add the CORS headers to the response
+    res.headers.append('Access-Control-Allow-Credentials', "true")
+    res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
+    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+    res.headers.append(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+
+    return res
 }
 
-export function middleware(request: NextRequest) {
-  if (request.method === 'OPTIONS') {
-    return NextResponse.json({}, { headers: corsHeaders })
-  }
-}
-
+// specify the path regex to apply the middleware to
 export const config = {
-  matcher: '/api/:path*',
+    matcher: '/api/:path*',
 }
