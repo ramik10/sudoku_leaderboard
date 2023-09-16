f0bfb0787267abc0c0e666360f618a5119627bb0
RAMIK MUKHERJEE <ramik.14062000@gmail.com>
2023-09-16 23:25:04 +0530

    init

--- src/app/api/winners/route.ts
+++ src/app/api/winners/route.ts
@@ -1,18 +1,20 @@
-
 import { NextRequest, NextResponse } from "next/server";
 import { connect } from "@/Db/connection";
 import { Winner } from "@/Db/schema";
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+
+export async function OPTIONS(req: NextRequest) {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
+
+
 connect();
 export async function POST(request: NextRequest) {
     const winners = await Winner.find();
     return NextResponse.json(winners, {status: 200});
   }
-  export const corsHeaders = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization",
-  };
-  
-  
-  export async function OPTIONS(req: NextRequest) {
-    return NextResponse.json({}, { headers: corsHeaders });}
