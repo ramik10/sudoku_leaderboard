d221a2c2a1f27dcc55f1652a2ab7ed0cb66ca300
RAMIK MUKHERJEE <ramik.14062000@gmail.com>
2023-09-16 23:07:44 +0530

    init

--- src/app/api/winners/route.ts
+++ src/app/api/winners/route.ts
@@ -1,8 +1,16 @@
 import { NextResponse } from "next/server";
 import { connect } from "@/Db/connection";
 import { Winner } from "@/Db/schema";
+import NextCors from 'nextjs-cors';
+import { NextApiRequest, NextApiResponse} from "next";
 connect();
-export async function GET(request: Request) {
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
+  await NextCors(req, res ,{
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+ });
     const winners = await Winner.find();
     return NextResponse.json(winners, {status: 200});
   }
